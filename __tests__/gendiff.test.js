504c2def52254135c9b10e0976d59170d5586f65
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-09-07 23:10:36 +0300

    add yml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,10 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-test('diff plain files', () => {
+test('diff json', () => {
   const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.json');
   const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.json');
-  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff.txt');
+  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-json.txt');
+  const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
+  const diff = gendiff(pathToFile1, pathToFile2);
+  expect(diffFromFile).toEqual(diff);
+});
+
+test('diff yml', () => {
+  const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.yml');
+  const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.yml');
+  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-yml.txt');
   const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
   const diff = gendiff(pathToFile1, pathToFile2);
   expect(diffFromFile).toEqual(diff);
