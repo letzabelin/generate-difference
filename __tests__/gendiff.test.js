105e21cb0c6b36f9e3e7988b8e49dd8efdb359b2
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-09-13 18:13:44 +0300

    add ini parser

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,19 +2,28 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-test('diff json', () => {
+test('diff plain json', () => {
   const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.json');
   const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.json');
-  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-json.txt');
+  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-plain.txt');
   const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
   const diff = gendiff(pathToFile1, pathToFile2);
   expect(diffFromFile).toEqual(diff);
 });
 
-test('diff yml', () => {
+test('diff plain yml', () => {
   const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.yml');
   const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.yml');
-  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-yml.txt');
+  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-plain.txt');
+  const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
+  const diff = gendiff(pathToFile1, pathToFile2);
+  expect(diffFromFile).toEqual(diff);
+});
+
+test('diff plain ini', () => {
+  const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.ini');
+  const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.ini');
+  const pathToResFile = path.resolve(__dirname, '__fixtures__/diff-plain.txt');
   const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
   const diff = gendiff(pathToFile1, pathToFile2);
   expect(diffFromFile).toEqual(diff);
